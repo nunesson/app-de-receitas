6324482f636f77add4d12ea27a374328d43c3612
Anderson Nunes <quatroovos@gmail.com>
2022-10-26 13:07:49 -0300

    requisitos 10, 11, 12, 13 e 14 do SearchBar

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -4,16 +4,32 @@ import Header from '../components/Header';
 import MyContext from '../context/MyContext';
 
 function Drinks() {
-  const { setHeaderTitle, setHideSearch } = useContext(MyContext);
+  const { setHeaderTitle,
+    setHideSearch, setRecipeType, results } = useContext(MyContext);
 
   useEffect(() => {
     setHeaderTitle('Drinks');
+    setRecipeType('thecocktaildb');
     setHideSearch(true);
   });
+
+  const NUMBER_12 = 12;
 
   return (
     <div>
       <Header />
+      {
+        results && results.slice(0, NUMBER_12).map((element, index) => (
+          <div key={ index } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ element.strDrinkThumb }
+              alt={ element.strDrink }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{element.strDrink}</p>
+          </div>
+        ))
+      }
       <Footer />
     </div>
   );
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -4,16 +4,32 @@ import Header from '../components/Header';
 import MyContext from '../context/MyContext';
 
 export default function Comidas() {
-  const { setHeaderTitle, setHideSearch } = useContext(MyContext);
+  const { setHeaderTitle,
+    setHideSearch, setRecipeType, results } = useContext(MyContext);
 
   useEffect(() => {
     setHeaderTitle('Meals');
+    setRecipeType('themealdb');
     setHideSearch(true);
   });
+
+  const NUMBER_12 = 12;
 
   return (
     <div>
       <Header />
+      {
+        results && results.slice(0, NUMBER_12).map((element, index) => (
+          <div key={ index } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ element.strMealThumb }
+              alt={ element.strMeal }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{element.strMeal}</p>
+          </div>
+        ))
+      }
       <Footer />
     </div>
   );
