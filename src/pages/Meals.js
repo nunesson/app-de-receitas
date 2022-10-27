b518ef579d1e12758835afe27a4f0fed0667b61b
Anderson Nunes <104839440+nunesson@users.noreply.github.com>
2022-10-27 08:39:22 -0300

    Update Meals.js

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,6 +1,7 @@
-// import Recipes from '../components/Recipes';
-import Header from '../components/Header';
+import React, { useContext, useEffect } from 'react';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
+import MyContext from '../context/MyContext';
 
 export default function Comidas() {
   const { setHeaderTitle,
