2cf2afd7e0c28b5302eee85027be211f9e472808
Mychel Albuquerque <mychelalbu@gmail.com>
2022-11-02 00:55:34 -0300

    inicia req 41

---
+++ src/components/ButtonShareInProgress.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+
+const copy = require('clipboard-copy');
+
+function ButtonShareInProgress() {
+  const [link, setlink] = useState(false);
+  const params = useParams();
+  const handleClick = () => {
+    copy(`http://localhost:3000/${params['meals' || 'drinks']}/${params.id}`);
+    setlink(true);
+    const time = 5000;
+    setTimeout(() => setlink(false), time);
+  };
+
+  return (
+    <>
+      { console.log(params) }
+      <button
+        data-testid="share-btn"
+        type="button"
+        onClick={ handleClick }
+      >
+        <img src={ shareIcon } alt="shareIcon" />
+      </button>
+      { link && <p>Link copied!</p> }
+    </>
+  );
+}
+
+export default ButtonShareInProgress;
